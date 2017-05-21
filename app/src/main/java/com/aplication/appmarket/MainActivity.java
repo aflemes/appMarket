4b9d155b164dc2f3a0dc4fc75142b308b0ce2d71
Allan Lemes <aflemess@gmail.com>
2017-05-21 18:35:42 -0300

    #adicionado commit no firebase

---
+++ app/src/main/java/com/aplication/appmarket/MainActivity.java
@@ -0,0 +1,10 @@
+package com.aplication.appmarket;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by skyli on 21/05/2017.
+ */
+
+public class MainActivity  extends AppCompatActivity {
+}
---
+++ app/src/main/java/model/User.java
@@ -0,0 +1,26 @@
+package model;
+
+/**
+ * Created by skyli on 21/05/2017.
+ */
+
+public class User {
+    private String email;
+    private String name;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
